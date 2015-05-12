03faa9e4aa4355fc9401c9f89b4bceb08d1efd91
Michael Silveira <gravitypersists@gmail.com>
2015-05-12 16:14:34 -0700

    improvements to input form item type

--- src/types/input.js
+++ src/types/input.js
@@ -3,14 +3,23 @@ const insert = require('../insert-html');
 module.exports = {
   defaults: {
     config: "",
-    placeholder: "Enter text"
+    placeholder: "Enter text",
+    label: ""
   },
-  render: function(element, schemaItem, data) {
+  render: function(element, schemaItem, data, smith) {
     let html = `
-      <input class='fs-input' type='text' value='${ data[schemaItem.key] }'>
+      <div class='fs-input-container'>
+        <span class='fs-input-label'>${ schemaItem.label }</span>
+        <span class='fs-input-wrapper'>
+          <input class='fs-input' 
+                 type='text'
+                 value='${ data[schemaItem.key] }'
+                 placeholder='${ schemaItem.placeholder }'>
+        </span>
+      </div>
     `
     let el = insert(element, html);
-    el.addEventListener('keyup', function(){
+    el.querySelector('input').addEventListener('keyup', function(){
       data[schemaItem.key] = this.value;
       smith.change();
     });
